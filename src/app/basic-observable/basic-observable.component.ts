703222641ed8cabd5300df8939f9d0b0afc542f1
teddy <teddy2@abv.bg>
2024-02-19 09:17:37 +0200

    Cleanup function after completion.

--- src/app/basic-observable/basic-observable.component.ts
+++ src/app/basic-observable/basic-observable.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-basic-observable',
@@ -7,15 +7,37 @@ import {Observable} from "rxjs";
   styleUrls: ['./basic-observable.component.scss']
 })
 export class BasicObservableComponent implements OnInit {
-  basicObservable: Observable<string> = new Observable(subscriber => {
-    subscriber.next('Teddy');
+  basicObservable: Observable<string> = new Observable(
+  subscriber => {
+    subscriber.next('Hello');
+    setTimeout(() => {
+      subscriber.next('Teddy');
+    }, 2500);
+
+    setTimeout(() => {
+      subscriber.next('Alice');
+    }, 5500);
+
+    /**
+     * Cleanup function to be called after the observable is complete.
+     */
+    return () => {
+      console.log('Teardown');
+    }
   });
+
+  subscription!: Subscription;
 
   ngOnInit(): void {
     const observer = {
       next: (message: string) => console.log(message)
     };
 
-    this.basicObservable.subscribe(observer);
+    this.subscription = this.basicObservable.subscribe(observer);
+
+    setTimeout(() => {
+      console.log('Unsubscribe');
+      this.subscription.unsubscribe();
+    }, 4500);
   }
 }
