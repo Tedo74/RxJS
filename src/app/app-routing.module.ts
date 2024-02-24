fa439fb3c47186793d3e4b159723e30659755b3f
teddy <teddy2@abv.bg>
2024-02-24 17:13:51 +0200

    Add simple navigation.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BasicObservableComponent} from "./basic-observable/basic-observable.component";
+import {HotObservableComponent} from "./hot-observable/hot-observable.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "basic",
+    component: BasicObservableComponent
+  },
+  {
+    path: "hot",
+    component: HotObservableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
